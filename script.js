90872ad32c479687e4c3df455374c8cd8ea109f8
MauroBorzi <mauroborzi91@gmail.com>
2025-07-01 15:23:03 +0200

    [add]prezzo18_65

--- script.js
+++ script.js
@@ -2,14 +2,22 @@
 const age = parseInt(prompt('Inserisci la tua età'))
 const km = parseInt(prompt('Inserisci i chilometri da percorrere'))
 const priceTicket = km * 0.21
-
+const priceChilds = priceTicket * (1 - 0.20)
+const priceOver = priceTicket * (1 - 0.40)
+const ageTwo = 18
+const ageThree = 65
 
 // sconto minorenni
 if(age < 18){
-  console.log(priceTicket * (1 - 0.20))
+  console.log(priceChilds)
 }
 
 // sconto over 65
 if(age > 65){
-  console.log(priceTicket * (1 - 0.40))
+  console.log(priceOver)
+}
+
+// prezzo età compresa 18-65
+if(age >= ageTwo && age <= ageThree ){
+  console.log(priceTicket)
 }
