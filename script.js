272e29ec6df0c8464f7200a1bce989ea09c136e5
MauroBorzi <mauroborzi91@gmail.com>
2025-07-01 14:38:25 +0200

    [add]priceTicket

---
+++ script.js
@@ -0,0 +1,7 @@
+// variable definitions
+const age = parseInt(prompt('Inserisci la tua età'))
+const km = parseInt(prompt('Inserisci i chilometri da percorrere'))
+
+// prezzo biglietto
+const priceTicket = km * 0.21
+console.log(priceTicket)
