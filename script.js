21ce4fc4b346efb7a73cfe0675652d38f5d43b3c
MauroBorzi <mauroborzi91@gmail.com>
2025-07-01 14:49:06 +0200

    [add]sconto-minorenni

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // variable definitions
 const age = parseInt(prompt('Inserisci la tua età'))
 const km = parseInt(prompt('Inserisci i chilometri da percorrere'))
-
-// prezzo biglietto
 const priceTicket = km * 0.21
-console.log(priceTicket)
+
+
+// sconto minorenni
+if(age < 18){
+  console.log(priceTicket * (1 - 0.20))
+}
+
